d786d5d1f792ed530c5303000b109169d3ad6881
Osmestrasmesnica <vlku_aleksa@hotmail.com>
2024-03-23 21:49:21 +0100

    Fine tunning

--- Dan_69-BlogNastavak/main.py
+++ Dan_69-BlogNastavak/main.py
@@ -10,6 +10,7 @@ from sqlalchemy import Integer, String, Text, ForeignKey
 from functools import wraps
 from werkzeug.security import generate_password_hash, check_password_hash
 import os
+from datetime import datetime
 # Import your forms from the forms.py
 from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
 
@@ -87,14 +88,15 @@ class Comment(db.Model):
     __tablename__ = "comments"
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     text: Mapped[str] = mapped_column(Text, nullable=False)
+    posted_time = db.Column(db.DateTime, nullable=False)
 
-    # Add child relationship
+    # ***************Child Relationship for User*************#
     #"users.id" The users refers to the tablename of the Users class.
     #"comments" refers to the comments property in the User class.
     author_id: Mapped[int] = mapped_column(Integer, db.ForeignKey("users.id"))
     comment_author = relationship("User", back_populates="comments")
 
-    #Child Relationship
+    # ***************Child Relationship for BlogPost*************#
     post_id: Mapped[int] = mapped_column(Integer, db.ForeignKey("blog_posts.id"))
     parent_post = relationship("BlogPost", back_populates="comments")
 
@@ -116,7 +118,7 @@ gravatar = Gravatar(app,
                     use_ssl=False,
                     base_url=None)
 
-#Create admin-only decorator
+# Create admin-only decorator
 def admin_only(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
@@ -126,6 +128,37 @@ def admin_only(f):
         #Otherwise continue with the route function
         return abort(403)        
     return decorated_function
+
+# You can allow the user who commented to delete the comment.
+def only_commenter(function):
+    @wraps(function)
+    def check(*args, **kwargs):
+        user = db.session.execute(db.select(Comment).where(Comment.author_id == current_user.id)).scalar()
+        if not current_user.is_authenticated or current_user.id != user.author_id:
+            return abort(403)
+        return function(*args, **kwargs)
+    return check
+
+def calculate_time_difference(posted_time):
+    current_time = datetime.now()
+    time_difference = current_time - posted_time
+
+    # Extract days, hours, and minutes
+    days = time_difference.days
+
+    # the divmod divide the time_difference.seconds by 3600 and then save the answer as a tuple, with the whole number and remainder, 
+    # as hours and remainder(minutes) respectively. the same as the second tuple, only that the remainder( _ ) which is the seconds is not needed.
+    hours, remainder = divmod(time_difference.seconds, 3600)
+    minutes, _ = divmod(remainder, 60)
+
+    if days > 0:
+        return f"{days} days ago"
+    elif hours > 0:
+        return f"{hours} hours ago"
+    elif minutes > 0:
+        return f"{minutes} minutes ago"
+    else:
+        return "just now"
 
 # TODO: Use Werkzeug to hash the user's password when creating a new user.
 @app.route('/register', methods=["GET", "POST"])
@@ -162,8 +195,6 @@ def register():
 
         # Log in and authenticate user with Flask-Login after adding details to the database
         login_user(new_user)
-
-        flash("Registration successful!", "success")
         return redirect(url_for("get_all_posts", godina=godina))
 
     return render_template("register.html", form=form, godina=godina, logged_in=current_user.is_authenticated)
@@ -226,13 +257,28 @@ def show_post(post_id):
         new_comment = Comment(
             text=form.comment_text.data,
             comment_author=current_user,
-            parent_post=requested_post
+            parent_post=requested_post,
+            posted_time=datetime.now(),
+            # author_id=current_user.id, # --> pošto sam postavio relationship onda ne mora ovo da se piše
+            # post_id=requested_post.id, # --> pošto sam postavio relationship onda ne mora ovo da se piše
         )
 
         db.session.add(new_comment)
         db.session.commit()
 
-    return render_template("post.html", form=form, post=requested_post, godina=godina, logged_in=current_user.is_authenticated, current_user=current_user)
+    # tracing the days, hours and minutes each comment is made
+    # list of comments with the same post_id
+    comments = Comment.query.filter_by(post_id=post_id).all()
+
+    # updating the time and turning it to list. note: this doesn't affect the data(time) in posted_time in the comment query
+    the_time = []
+    for comments_time in comments:
+        print(comments_time.posted_time)
+        time = calculate_time_difference(comments_time.posted_time)
+        the_time.append(time)
+    print(the_time)
+
+    return render_template("post.html", form=form, post=requested_post, godina=godina, logged_in=current_user.is_authenticated, current_user=current_user, posted_time=the_time)
 
 
 # TODO: Use a decorator so only an admin user can create a new post
@@ -287,6 +333,15 @@ def delete_post(post_id):
     db.session.commit()
     return redirect(url_for('get_all_posts'))
 
+# The function for deleting comments if you are author
+@app.route("/delete/comment/<int:id>/<int:post_id>")
+@only_commenter
+def delete_comment(post_id, id):
+    post_to_delete = db.get_or_404(Comment, id)
+    db.session.delete(post_to_delete)
+    db.session.commit()
+    return redirect(url_for('show_post', post_id=post_id, logged_in=current_user.is_authenticated))
+
 
 @app.route("/about")
 def about():
@@ -300,3 +355,7 @@ def contact():
 
 if __name__ == "__main__":
     app.run(debug=True, port=5002)
+
+#! PROVERITI KADA BUDES NEKADA IMAO VREMENA ZASTO OVO NE RADI LEPO KADA OBRISES "posts.db"
+#! IZBACUJE TI ERROR 404
+#! SKONTAO SI DA TREBA DA SE UBACI USER PREKO DB BROWSERa(ti si morao nekoliko njih da ubacis) DA BI PRORADILO
